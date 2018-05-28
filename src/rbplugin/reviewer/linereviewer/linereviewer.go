179a5dbd993d4c84a0df86bae2d03a0f4009fae7
FifteenFifty <matt.bissenden@gmail.com>
2018-05-28 11:35:38 +0100

    Put line reviewer back to 80 char check

--- src/rbplugin/reviewer/linereviewer/linereviewer.go
+++ src/rbplugin/reviewer/linereviewer/linereviewer.go
@@ -15,7 +15,7 @@ func CheckLength(diffChunk   reviewdata.DiffChunk,
     comment.NumLines = 0
 
     for _, line := range diffChunk.Lines {
-        if (len(line.RhText) > 8) {
+        if (len(line.RhText) > 80) {
             comment.NumLines++
 
             if (comment.NumLines == 1) {
